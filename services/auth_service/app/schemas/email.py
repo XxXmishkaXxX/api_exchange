d5a33c507fc619a5ef44436a1a043a9161db7ecc
Your Name <you@example.com>
2025-02-19 18:25:44 +0500

    add ResendVerificationRequest schema

--- services/auth_service/app/schemas/email.py
+++ services/auth_service/app/schemas/email.py
@@ -3,3 +3,7 @@ from pydantic import BaseModel, EmailStr
 class VerificationRequest(BaseModel):
     email: EmailStr 
     verification_code: str
+
+
+class ResendVerificationRequest(BaseModel):
+    email: EmailStr
