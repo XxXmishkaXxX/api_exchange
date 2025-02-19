992e2795df1a02b8f7ec71431acd815b6b7d0ba9
Your Name <you@example.com>
2025-02-19 15:01:58 +0500

    fix email column

--- services/auth_service/app/models/user.py
+++ services/auth_service/app/models/user.py
@@ -5,7 +5,7 @@ class User(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True)
-    email = Column(String, primary_key=True, index=True, nullable=False, unique=True)
+    email = Column(String, nullable=False, unique=True)
     name = Column(String, nullable=False)
     password = Column(String, nullable=True)
     oauth_provider = Column(String, nullable=True)  
