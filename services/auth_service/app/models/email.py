22a1165a0cb62573143d393764f7256f2b53c04b
Your Name <you@example.com>
2025-02-19 17:30:02 +0500

    fix imports

--- services/auth_service/app/models/email.py
+++ services/auth_service/app/models/email.py
@@ -1,7 +1,7 @@
-from sqlalchemy import Column, Integer, String, ForeignKey, DateTime, 
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
 from sqlalchemy.orm import relationship
 from sqlalchemy.sql import func
-from database import Base
+from app.db.database import Base
 
 
 
