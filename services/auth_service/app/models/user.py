3d4b01411daeaa43bd5995ac236ae3e8084032b2
Your Name <you@example.com>
2025-02-19 18:02:36 +0500

    fix relationship

--- services/auth_service/app/models/user.py
+++ services/auth_service/app/models/user.py
@@ -1,5 +1,7 @@
 from sqlalchemy import Column, String, Integer, Boolean
 from app.db.database import Base
+from sqlalchemy.orm import relationship
+
 
 class User(Base):
     __tablename__ = "users"
@@ -11,3 +13,5 @@ class User(Base):
     oauth_provider = Column(String, nullable=True)  
     oauth_id = Column(String, nullable=True, unique=True)
     is_verified = Column(Boolean, default=False)
+
+    email_verifications = relationship("EmailVerification", back_populates="user")
