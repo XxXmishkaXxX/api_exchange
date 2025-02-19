ae7089175e12b3a29c36ce588784b2c3deae7d22
Your Name <you@example.com>
2025-02-19 14:26:40 +0500

    add id and is_verified colums

--- services/auth_service/app/models/user.py
+++ services/auth_service/app/models/user.py
@@ -1,11 +1,13 @@
-from sqlalchemy import Column, String
+from sqlalchemy import Column, String, Integer, Boolean
 from app.db.database import Base
 
 class User(Base):
     __tablename__ = "users"
 
+    id = Column(Integer, primary_key=True, index=True)
     email = Column(String, primary_key=True, index=True, nullable=False, unique=True)
     name = Column(String, nullable=False)
     password = Column(String, nullable=True)
     oauth_provider = Column(String, nullable=True)  
-    oauth_id = Column(String, nullable=True, unique=True) 
+    oauth_id = Column(String, nullable=True, unique=True)
+    is_verified = Column(Boolean, default=False)
