3d4902e4a1fc3c77808a47c740ee1ba64765b8a4
Your Name <you@example.com>
2025-02-20 18:38:38 +0500

    add endpoint to resend verification code

--- services/auth_service/app/routers/email.py
+++ services/auth_service/app/routers/email.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, HTTPException, Depends
 from app.services.email import VerificationEmailService
 from app.db.database import get_db
-from app.schemas.email import VerificationRequest
+from app.schemas.email import VerificationRequest, ResendVerificationRequest
 from sqlalchemy.ext.asyncio import AsyncSession
 
 router = APIRouter()
@@ -14,17 +14,9 @@ async def verify_email(data: VerificationRequest, db: AsyncSession = Depends(get
         raise HTTPException(status_code=400, detail="Invalid verification code")
     return {"message": "Verification successful"}
 
-# @router.post("/resend_verification_code", response_model=dict)
-# async def resend_verification_code(
-#     resend_request: ResendVerificationRequest,
-#     user_repo: UserRepository = Depends(get_db),  # Получаем репозиторий через зависимость
-#     verification_service: VerificationEmail = Depends(VerificationEmail),
-# ):
-#     """
-#     Повторная отправка кода подтверждения на почту.
-#     """
-#     try:
-#         result = await verification_service.resend_verification_email_code(resend_request.user_id)
-#         return result
-#     except HTTPException as e:
-#         raise e
+
+@router.post("/resend_verification_code", response_model=dict)
+async def resend_verification_code(data: ResendVerificationRequest, db: AsyncSession = Depends(get_db)):
+    service = VerificationEmailService(db)
+    result = await service.resend_verification_email_code(data)
+    return result
