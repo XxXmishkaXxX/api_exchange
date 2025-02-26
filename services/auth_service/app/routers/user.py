b379b96a42fb0ae206eac29531b81d9450d65f68
Your Name <you@example.com>
2025-02-26 21:42:39 +0500

    add reset routes

--- services/auth_service/app/routers/user.py
+++ services/auth_service/app/routers/user.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Depends, HTTPException, status, Security
 from sqlalchemy.ext.asyncio import AsyncSession
 
 
-from app.schemas.user import ChangePasswordRequest
+from app.schemas.user import ChangePasswordRequest, ForgotPasswordRequest, ResetCodeRequest
 from app.db.database import get_db
 from app.services.auth import oauth2_scheme
 from app.services.user import UserService
@@ -21,3 +21,22 @@ async def chahge_password(data: ChangePasswordRequest,
     user = await service.get_current_user(token=token)
     await service.change_password(user.id, data)
     return {"message": "Password has changed"}
+
+
+@router.post("/forgot-password", response_model=dict)
+async def forgot_password(data: ForgotPasswordRequest, 
+                          db: AsyncSession = Depends(get_db)
+                          ):
+    service = UserService(db)
+    await service.forgot_password(data=data)
+    return {"message": "Code has send to email"}
+
+
+@router.post("/confirm-reset-code", response_model=dict)
+async def confirm_reset_code(data: ResetCodeRequest, db: AsyncSession = Depends(get_db)):
+    service = UserService(db)
+    await service.confirm_reset_code(data=data)
+    return {"message": "Password has reset"}
+
+
+
