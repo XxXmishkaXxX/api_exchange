b3c691be33f296c4843d788cc4c64ab64d705561
Your Name <you@example.com>
2025-02-26 23:21:34 +0500

    refactor routers

--- services/auth_service/app/routers/email.py
+++ services/auth_service/app/routers/email.py
@@ -1,19 +1,18 @@
-from fastapi import APIRouter, HTTPException, Depends
-from app.services.email import VerificationEmailService
-from app.db.database import get_db
+from fastapi import APIRouter, Depends
+from app.services.email import VerificationEmailService, get_email_service
 from app.schemas.email import VerificationRequest, ResendVerificationRequest
-from sqlalchemy.ext.asyncio import AsyncSession
+
 
 router = APIRouter()
 
 @router.post("/verify_email", response_model=dict)
-async def verify_email(data: VerificationRequest, db: AsyncSession = Depends(get_db)):
-    service = VerificationEmailService(db)
-    result = await service.verify_email_code(data)
-    return result
+async def verify_email(data: VerificationRequest, 
+                       service: VerificationEmailService = Depends(get_email_service)
+                       ) -> dict:
+    return await service.verify_email_code(data)
 
 @router.post("/resend_verification_code", response_model=dict)
-async def resend_verification_code(data: ResendVerificationRequest, db: AsyncSession = Depends(get_db)):
-    service = VerificationEmailService(db)
-    result = await service.resend_verification_email_code(data)
-    return result
+async def resend_verification_code(data: ResendVerificationRequest, 
+                                   service: VerificationEmailService = Depends(get_email_service)
+                                   ) -> dict:
+    return await service.resend_verification_email_code(data)
