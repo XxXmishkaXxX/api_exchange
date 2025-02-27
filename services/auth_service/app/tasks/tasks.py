7470f333a7d7637b2b9d3a3c76ca249096c1996f
Your Name <you@example.com>
2025-02-27 16:46:23 +0500

    add task for sending email

--- services/auth_service/app/tasks/tasks.py
+++ services/auth_service/app/tasks/tasks.py
@@ -1,15 +1,20 @@
-# tasks.py
 import asyncio
-from app.tasks.worker_config import app
+from datetime import datetime, timedelta
 from sqlalchemy.future import select
+from fastapi_mail import FastMail, MessageSchema
+
+from app.tasks.worker_config import app
 from app.models.user import User
 from app.db.database import get_db
-from datetime import datetime, timedelta
+from app.core.config import email_conf
+
+
+mail = FastMail(email_conf)
+
 
 @app.task
 def delete_inactive_users():
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(run_delete_inactive_users())
+    asyncio.run(run_delete_inactive_users())
 
 async def run_delete_inactive_users():
     async for session in get_db():
@@ -29,3 +34,18 @@ async def run_delete_inactive_users():
 
         await session.commit()
         print(f"Deleted {len(users_to_delete)} inactive users.")
+        break
+
+
+@app.task
+def send_email_task(message: dict):
+    asyncio.run(send_email(message))
+
+async def send_email(message: dict):
+    email_message = MessageSchema(
+        subject=message["subject"],
+        recipients=message["recipients"],
+        body=message["body"],
+        subtype=message["subtype"]
+    )
+    await mail.send_message(email_message)
