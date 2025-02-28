79e1a5c38285f95d7cf6c8276337b0a5ca87747b
Your Name <you@example.com>
2025-02-28 17:54:39 +0500

    ref auth schemas

--- services/auth_service/app/schemas/auth.py
+++ services/auth_service/app/schemas/auth.py
@@ -1,13 +1,26 @@
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel, EmailStr, Field, field_validator
+import re
 
 class RegisterRequest(BaseModel):
     email: EmailStr
-    name: str
-    password: str
+    name: str = Field(..., min_length=2, max_length=50, pattern=r"^[A-Za-zА-Яа-яЁё]+$")
+    password: str = Field(..., min_length=8, max_length=128)
+
+    @field_validator("password")
+    def validate_password(cls, value: str) -> str:
+        if not re.search(r"[a-z]", value):
+            raise ValueError("Пароль должен содержать хотя бы одну строчную букву.")
+        if not re.search(r"[A-Z]", value):
+            raise ValueError("Пароль должен содержать хотя бы одну заглавную букву.")
+        if not re.search(r"\d", value):
+            raise ValueError("Пароль должен содержать хотя бы одну цифру.")
+        if not re.search(r"[@$!%*?&]", value):
+            raise ValueError("Пароль должен содержать хотя бы один специальный символ (@$!%*?&).")
+        return value
 
 class LoginRequest(BaseModel):
     email: EmailStr
-    password: str
+    password: str = Field(..., min_length=8, max_length=128)
 
 class Token(BaseModel):
     access_token: str
