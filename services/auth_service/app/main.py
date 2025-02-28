62d0d3d6f2ea9473d2f875de956420aeb8c61229
Your Name <you@example.com>
2025-02-28 16:47:14 +0500

    little fix

--- services/auth_service/app/main.py
+++ services/auth_service/app/main.py
@@ -4,10 +4,7 @@ from slowapi.middleware import SlowAPIMiddleware
 from starlette.middleware.sessions import SessionMiddleware
 from starlette.responses import JSONResponse
 
-from app.routers.auth import router as auth_router
-from app.routers.oauth2 import router as oauth2_router
-from app.routers.email import router as email_router
-from app.routers.user import router as user_router
+from app.routers import auth, oauth2, email, user
 from app.db.database import engine, Base
 from app.core.config import settings
 from app.core.limiter import limiter
@@ -39,10 +36,10 @@ async def rate_limit_handler(request, exc):
 
 app.add_middleware(SessionMiddleware, secret_key=settings.SESSION_KEY)
 
-app.include_router(auth_router, prefix="/api/v1/auth", tags=["auth"])
-app.include_router(oauth2_router, prefix="/api/v1/oauth", tags=["oauth"])
-app.include_router(email_router, prefix="/api/v1/mail", tags=["mail"])
-app.include_router(user_router, prefix="/api/v1/user", tags=["user"])
+app.include_router(auth.router, prefix="/api/v1/auth", tags=["auth"])
+app.include_router(oauth2.router, prefix="/api/v1/oauth", tags=["oauth"])
+app.include_router(email.router, prefix="/api/v1/mail", tags=["mail"])
+app.include_router(user.router, prefix="/api/v1/user", tags=["user"])
 
 if __name__ == "__main__":
     import uvicorn
