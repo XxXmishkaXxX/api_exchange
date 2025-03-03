29bf9eef214eb53a5f27aca13d7206400d4b014c
XxXmishkaXxX <misha.kutsov1@gmail.com>
2025-03-03 18:56:23 +0500

    little fix

--- services/auth_service/app/models/email.py
+++ services/auth_service/app/models/email.py
@@ -11,7 +11,7 @@ class EmailVerification(Base):
     id = Column(Integer, primary_key=True, index=True)
     user_id = Column(Integer, ForeignKey('users.id'))
     user_email = Column(String, nullable=False, unique=True)
-    verification_code = Column(String, index=True)
+    verification_code = Column(String, nullable=False)
     created_at = Column(DateTime, server_default=func.now())
     expires_at = Column(DateTime)
 
