c18436c8de3c929c5fe8d5ad9b70f1d69f78423e
XxXmishkaXxX <misha.kutsov1@gmail.com>
2025-03-13 20:16:11 +0500

    add order repo

---
+++ services/orders_service/app/repositories/order_repo.py
@@ -0,0 +1,21 @@
+from sqlalchemy.ext.asyncio import AsyncSession
+
+
+
+class OrderRepository:
+    
+    def __init__(self, session: AsyncSession):
+        pass
+
+    async def get_order():
+        pass
+
+    async def get_orders_list():
+        pass
+
+    async def create_order():
+        pass
+
+    async def cancel_order():
+        pass
+    
