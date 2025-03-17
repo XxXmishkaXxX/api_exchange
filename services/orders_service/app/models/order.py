bb49fa12109ebc16227097f779d5a7f41dbc8fea
XxXmishkaXxX <misha.kutsov1@gmail.com>
2025-03-17 13:02:35 +0500

    add order status

--- services/orders_service/app/models/order.py
+++ services/orders_service/app/models/order.py
@@ -1,14 +1,8 @@
-from sqlalchemy import Column, Integer, String, Float, Enum
-from enum import Enum as PyEnum
+from sqlalchemy import Column, Integer, ForeignKey, Enum, Float
+from sqlalchemy.orm import relationship
 from app.db.database import Base
+from app.schemas.order import StatusOrder, Direction, OrderType
 
-class Direction(str, PyEnum):
-    BUY = "buy"
-    SELL = "sell"
-
-class OrderType(str, PyEnum):
-    MARKET = "market"
-    LIMIT = "limit"
 
 class Order(Base):
     __tablename__ = "orders"
@@ -16,10 +10,13 @@ class Order(Base):
     id = Column(Integer, primary_key=True, autoincrement=True)
     user_id = Column(Integer, nullable=False)
     type = Column(Enum(OrderType), nullable=False)
+    status = Column(Enum(StatusOrder), nullable=False, default="new")
     direction = Column(Enum(Direction), nullable=False)
-    ticker = Column(String(10), nullable=False)
+    ticker_id = Column(Integer, ForeignKey("tickers.id"), nullable=False)
     qty = Column(Integer, nullable=False)
     price = Column(Float, nullable=True)
 
+    ticker = relationship("Ticker", back_populates="orders")
+
     def __repr__(self):
-        return f"<Order(id={self.id}, type={self.type}, direction={self.direction}, ticker={self.ticker}, qty={self.qty}, price={self.price})>"
+        return f"<Order(id={self.id}, type={self.type}, direction={self.direction}, ticker_id={self.ticker_id}, qty={self.qty}, price={self.price})>"
