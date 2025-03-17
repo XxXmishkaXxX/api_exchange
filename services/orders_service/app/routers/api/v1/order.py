efeeb6a780bfdb1bcbe0ccb153395a30f43ee7d2
XxXmishkaXxX <misha.kutsov1@gmail.com>
2025-03-17 13:03:39 +0500

    ref routes

--- services/orders_service/app/routers/api/v1/order.py
+++ services/orders_service/app/routers/api/v1/order.py
@@ -3,9 +3,9 @@ from fastapi import APIRouter, Depends, HTTPException, Security
 from fastapi.security import OAuth2PasswordBearer
 from jose import jwt, JWTError
 
-from app.schemas.order import Order, OrderResponse, OrderListResponse
+from app.schemas.order import OrderSchema, OrderCreateResponse, OrderListResponse, OrderResponse, OrderCancelResponse
 from app.core.config import settings
-from app.service.order import OrderService, get_order_servcice
+from app.services.order import OrderService, get_order_servcice
 
 router = APIRouter()
 
@@ -24,30 +24,24 @@ def verify_token(token: str = Security(oauth2_scheme)):
 @router.get("/", response_model=OrderListResponse)
 async def get_orders_list(user_data: dict = Depends(verify_token),
                           service: OrderService = Depends(get_order_servcice)):
-    print(user_data)
     return await service.get_list_order(user_data)
 
 
-@router.post("/", response_model=OrderResponse)
-async def create_order(order: Order, 
+@router.post("/", response_model=OrderCreateResponse)
+async def create_order(order: OrderSchema, 
                        user_data: dict = Depends(verify_token),  
-                       service: OrderService = Depends(get_order_servcice) ):
-    print(user_data)
-    order_id = await service.create_order(user_data, order)
-    return OrderResponse(order_id=order_id, **order.dict())
+                       service: OrderService = Depends(get_order_servcice)):
+    return await service.create_order(user_data, order)
 
 
 @router.get("/{order_id}", response_model=OrderResponse)
-async def get_order(order_id: str,
+async def get_order(order_id: int,
                     user_data: dict = Depends(verify_token),  
                     service: OrderService = Depends(get_order_servcice)):
-    
-    order = await service.get_order(user_data, order_id)
-    return OrderResponse(**order)
+    return await service.get_order(user_data, order_id)
 
-@router.delete("/{order_id}")
-async def cancel_order(order_id: str, 
+@router.delete("/{order_id}", response_model=OrderCancelResponse)
+async def cancel_order(order_id: int, 
                        user_data: dict = Depends(verify_token),
                        service: OrderService = Depends(get_order_servcice)):
-    await service.cancel_order(user_data, order_id)
-    return {"message": "Order cancelled successfully"}
+    return await service.cancel_order(user_data, order_id) 
