fe799d073bbeb185ce87d4885d6eaf787bdf7ed4
XxXmishkaXxX <misha.kutsov1@gmail.com>
2025-03-22 16:20:05 +0500

    add producer

--- services/matching_engine/main.py
+++ services/matching_engine/main.py
@@ -1,13 +1,20 @@
 import asyncio
 import logging
 from kafka.consumer import KafkaConsumerService
+from kafka.producer import KafkaProducerService
+from engine.matching_engine import MatchingEngine
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 async def main():
     try:
-        consumer_service = KafkaConsumerService()
+        producer = KafkaProducerService()
+        await producer.start()
+
+        engine = MatchingEngine(producer)
+        
+        consumer_service = KafkaConsumerService(engine)
         await consumer_service.start()
     except Exception as e:
         logger.error(f"❌ Application error: {e}")
