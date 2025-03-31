3107dc4e9242b43b8645a9c069137740c82ccc20
XxXmishkaXxX <misha.kutsov1@gmail.com>
2025-03-31 15:38:44 +0500

    lil fix

--- services/market_data_service/alembic/env.py
+++ services/market_data_service/alembic/env.py
@@ -29,17 +29,6 @@ else:
 
 target_metadata = Base.metadata
 
-# add your model's MetaData object here
-# for 'autogenerate' support
-# from myapp import mymodel
-# target_metadata = mymodel.Base.metadata
-target_metadata = None
-
-# other values from the config, defined by the needs of env.py,
-# can be acquired:
-# my_important_option = config.get_main_option("my_important_option")
-# ... etc.
-
 
 def run_migrations_offline() -> None:
     """Run migrations in 'offline' mode.
