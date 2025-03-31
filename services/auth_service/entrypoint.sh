d676885854aed51e33ba22c80e905243d0e83000
XxXmishkaXxX <misha.kutsov1@gmail.com>
2025-03-31 15:44:13 +0500

    add entrypoints for services

---
+++ services/auth_service/entrypoint.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+# Функция ожидания доступности базы данных
+wait_for_db() {
+  echo "Ожидание доступности БД $1..."
+  until nc -z -v -w30 $1 5432
+  do
+    echo "Ожидание БД $1..."
+    sleep 1
+  done
+}
+
+# Ждем доступности md_db
+wait_for_db db
+
+# Проверяем наличие миграций для md_db
+if [ -z "$(ls -A migrations/versions/ 2>/dev/null)" ]; then
+  echo "Миграции не найдены, создаем первую миграцию..."
+  alembic revision --autogenerate -m "Initial migration"
+fi
+
+# Применяем миграции для md_db
+alembic upgrade head
+
+# Запускаем приложение
+exec uvicorn app.main:app --host 0.0.0.0 --port 8001 --reload
