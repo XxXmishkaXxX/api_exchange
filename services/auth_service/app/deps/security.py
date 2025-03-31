b1f07c9f5afc649996ed710a635e0901d1666956
XxXmishkaXxX <misha.kutsov1@gmail.com>
2025-03-31 19:43:49 +0500

    add exceptions

--- services/auth_service/app/deps/security.py
+++ services/auth_service/app/deps/security.py
@@ -1,26 +1,30 @@
 from fastapi import Depends, HTTPException, Security
 import jwt
 from fastapi.security import OAuth2PasswordBearer
+from jwt.exceptions import ExpiredSignatureError, InvalidSignatureError, PyJWTError
 
 from app.core.config import settings
-
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="api/v1/auth/login")
 
 def get_user_from_token(token: str = Security(oauth2_scheme)) -> dict:
     try:
-        print(token)
         payload = jwt.decode(token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM])
         user_id: str = payload.get("sub")
         role: str = payload.get("role")
         
-        if user_id is None or role is None:
-            raise HTTPException(status_code=401, detail="User or role not found in token")
+        if not user_id or not role:
+            raise HTTPException(status_code=401, detail="Invalid token payload")
         
         return {"user_id": int(user_id), "role": role}
-    except Exception as e:
+    
+    except ExpiredSignatureError:
+        raise HTTPException(status_code=401, detail="Token has expired")
+    except InvalidSignatureError:
+        raise HTTPException(status_code=401, detail="Invalid token signature")
+    except PyJWTError:
         raise HTTPException(status_code=401, detail="Invalid token")
 
 def admin_required(user_info: dict = Depends(get_user_from_token)) -> None:
-    if user_info['role'] != "admin":
+    if user_info["role"] != "admin":
         raise HTTPException(status_code=403, detail="You do not have permission to access this resource")
