869c51a75787cb913b3c6b320c48f6b42f7a16d8
XxXmishkaXxX <misha.kutsov1@gmail.com>
2025-03-31 14:36:19 +0500

    start md service

---
+++ services/market_data_service/app/core/config.py
@@ -0,0 +1,28 @@
+import os
+from pydantic_settings import BaseSettings
+from passlib.context import CryptContext
+
+
+class Settings(BaseSettings):
+    # Конфигурация для подключения к базе данных
+    DATABASE_URL: str 
+
+    # Секретный ключ для подписания JWT токенов
+    SECRET_KEY: str 
+    SESSION_KEY: str
+
+    # Логирование
+    LOG_LEVEL: str = "INFO"
+
+    # Другие переменные окружения
+    DEBUG: bool = False
+    TESTING: bool = False
+
+    class Config:
+        env_file = os.path.join(os.path.dirname(__file__), '..', '..', '.env')
+
+
+settings = Settings()
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
