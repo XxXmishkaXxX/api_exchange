5ce2507e83b1828455d3aed8046f7a9f7c583f90
XxXmishkaXxX <misha.kutsov1@gmail.com>
2025-04-01 18:17:56 +0500

    rename alembic dir

--- services/wallet_service/migrations/env.py
+++ services/wallet_service/migrations/env.py
@@ -8,6 +8,7 @@ from sqlalchemy.ext.asyncio import async_engine_from_config
 from alembic import context
 from app.core.config import settings
 from app.db.database import Base
+from app.models.wallet import UserAssetBalance
 
 # Get the Alembic Config object, which provides access to the values in the .ini file
 config = context.config
