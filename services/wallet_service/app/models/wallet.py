99769ba1f02c1a74f33e91d32b3ec02b567ce090
XxXmishkaXxX <misha.kutsov1@gmail.com>
2025-04-03 14:39:30 +0500

    ref column name

--- services/wallet_service/app/models/wallet.py
+++ services/wallet_service/app/models/wallet.py
@@ -6,10 +6,10 @@ class UserAssetBalance(Base):
     __tablename__ = "user_asset_balances"
 
     user_id = Column(Integer, nullable=False, primary_key=True)
-    ticker_id = Column(String, nullable=False, primary_key=True)
+    ticker = Column(String, nullable=False, primary_key=True)
     amount = Column(Float, default=0.0)
     locked = Column(Float, default=0.0)
 
     __table_args__ = (
-        Index("idx_user_assets_user_ticker", "user_id", "ticker_id")
+        Index("idx_user_assets_user_ticker", "user_id", "ticker")
     )
