148b65e35f141109fe2e2e716e7630007342c4e4
XxXmishkaXxX <misha.kutsov1@gmail.com>
2025-04-03 14:38:37 +0500

    connect consumers

--- services/wallet_service/app/main.py
+++ services/wallet_service/app/main.py
@@ -1,27 +1,39 @@
+import asyncio
+from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from starlette.middleware.sessions import SessionMiddleware
 
-from app.routers.api.v1 import wallet
-from app.db.database import engine, Base
+from app.routers.api.v1 import wallet_users, wallet_admins
 from app.core.config import settings
+from app.core.logger import logger
+from app.services.consumers import change_assets_consumer, lock_assets_consumer
 
 
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    try:
+        await change_assets_consumer.start()
+        asyncio.create_task(change_assets_consumer.consume_messages())
+        await lock_assets_consumer.start()
+        asyncio.create_task(lock_assets_consumer.consume_messages())
+        logger.info("✅ Kafka Consumers started.")
+    except Exception as e:
+        logger.warning(f"⚠️ Ошибка Kafka: {e}")
 
-app = FastAPI(title="Wallet Service")
+    yield
+
+    await change_assets_consumer.stop()
+    await lock_assets_consumer.stop()
+    logger.info("🛑 Consumers stopped.")
 
 
-async def create_tables():
-    async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all)
-
-@app.on_event("startup")
-async def on_startup():
-    await create_tables()
+app = FastAPI(title="Wallet Service", lifespan=lifespan)
 
 
 app.add_middleware(SessionMiddleware, secret_key=settings.SESSION_KEY)
 
-app.include_router(wallet.router, prefix="/api/v1/wallet", tags=["wallet"])
+app.include_router(wallet_users.router, prefix="/api/v1/balance", tags=["wallet"])
+app.include_router(wallet_admins.router, prefix="/api/v1/admin/balance", tags=["wallet"])
 
 if __name__ == "__main__":
     import uvicorn
