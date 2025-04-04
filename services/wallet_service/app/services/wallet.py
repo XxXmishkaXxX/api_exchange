393a39e60ad1aa594802fefd4adeb1cc5aa2f441
XxXmishkaXxX <misha.kutsov1@gmail.com>
2025-04-05 00:46:09 +0500

    ref

--- services/wallet_service/app/services/wallet.py
+++ services/wallet_service/app/services/wallet.py
@@ -5,6 +5,7 @@ from app.repositories.wallet_repo import WalletRepository
 from app.repositories.asset_repo import AssetRepository
 from app.db.database import get_db
 from app.schemas.wallet import WithdrawAssetsSchema, DepositAssetsSchema
+from app.services.producers import ChangeBalanceKafkaProducerService
 
 class WalletService:
     def __init__(self, wallet_repo: WalletRepository):
@@ -13,19 +14,39 @@ class WalletService:
     async def get_all_assets_balance(self, user_id: int):
         return await self.wallet_repo.get_all(user_id)
 
-    async def deposit_assets_user(self, data: DepositAssetsSchema):
+    async def deposit_assets_user(self, data: DepositAssetsSchema, 
+                                  prod: ChangeBalanceKafkaProducerService):
         asset = await self.wallet_repo.get(data.user_id, data.ticker)
         if not asset:
             await self.wallet_repo.create(data.user_id, data.ticker, data.amount)
         else:
             await self.wallet_repo.deposit(asset, data.amount)
+
+        message = {
+            "action": "deposit",
+            "user_id": data.user_id,
+            "asset": data.ticker,
+            "amount": data.amount 
+        }
+        await prod.send_message(message)
+        
         return {"success": True}
 
-    async def withdraw_assets_user(self, data: WithdrawAssetsSchema):
+    async def withdraw_assets_user(self, data: WithdrawAssetsSchema,
+                                   prod: ChangeBalanceKafkaProducerService):
         asset = await self.wallet_repo.get(data.user_id, data.ticker)
         if not asset:
             raise ValueError("Актив не найден")
         await self.wallet_repo.withdraw(asset, data.amount)
+
+        message = {
+            "action": "withdraw",
+            "user_id": data.user_id,
+            "asset": data.ticker,
+            "amount": data.amount 
+        }
+        
+        await prod.send_message(message)
         return {"success": True}
 
 
