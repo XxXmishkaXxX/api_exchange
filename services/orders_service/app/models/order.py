369dac7b021795192ec8b50f7866d58b79f1b9ab
XxXmishkaXxX <misha.kutsov1@gmail.com>
2025-04-06 19:24:39 +0500

    ref order model

--- services/orders_service/app/models/order.py
+++ services/orders_service/app/models/order.py
@@ -1,6 +1,8 @@
-from sqlalchemy import Column, Integer, Enum, Float
+from sqlalchemy import Column, Integer, Enum, Float, ForeignKey
+from sqlalchemy.orm import relationship
 from app.db.database import Base
 from app.schemas.order import StatusOrder, Direction, OrderType
+from app.models.asset import Asset
 
 
 class Order(Base):
@@ -11,10 +13,12 @@ class Order(Base):
     type = Column(Enum(OrderType), nullable=False)
     status = Column(Enum(StatusOrder), nullable=False, default="new")
     direction = Column(Enum(Direction), nullable=False)
-    ticker_id = Column(Integer, nullable=False)
+    
+    asset_id = Column(Integer, ForeignKey("assets.id"), nullable=False)
+    asset = relationship("Asset", back_populates="orders")
+
     qty = Column(Integer, nullable=False)
     price = Column(Float, nullable=True)
 
-
     def __repr__(self):
-        return f"<Order(id={self.id}, type={self.type}, direction={self.direction}, ticker_id={self.ticker_id}, qty={self.qty}, price={self.price})>"
+        return f"<Order(id={self.id}, type={self.type}, direction={self.direction}, ticker={self.asset.ticker}, qty={self.qty}, price={self.price})>"
