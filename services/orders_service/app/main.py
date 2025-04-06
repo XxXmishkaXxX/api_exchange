063c46e34abdd6ab8d68f1812ad621170b73273e
XxXmishkaXxX <misha.kutsov1@gmail.com>
2025-04-06 15:28:15 +0500

    lil ref

--- services/orders_service/app/main.py
+++ services/orders_service/app/main.py
@@ -1,5 +1,4 @@
 import asyncio
-import logging
 from fastapi import FastAPI
 from starlette.middleware.sessions import SessionMiddleware
 from contextlib import asynccontextmanager
@@ -7,28 +6,43 @@ from contextlib import asynccontextmanager
 from app.routers.api.v1 import order
 from app.core.config import settings
 from app.core.logger import logger
-from app.services.producer import producer_service
-from app.services.consumer import order_status_consumer, ticker_consumer
+from app.services.producer import lock_assets_producer, order_producer
+from app.services.consumer import order_status_consumer, asset_consumer, change_balance_consumer
+from app.db.database import redis_pool
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     try:
+        await redis_pool.start()
+        logger.info("✅ Redis started.")
+        
         await order_status_consumer.start()
         asyncio.create_task(order_status_consumer.consume_messages())
-        await ticker_consumer.start()
-        asyncio.create_task(ticker_consumer.consume_messages())
+        await asset_consumer.start()
+        asyncio.create_task(asset_consumer.consume_messages())
+        await change_balance_consumer.start()
+        asyncio.create_task(change_balance_consumer.consume_messages())
         logger.info("✅ Kafka Consumers started.")
-        await producer_service.start()
-        logger.info("✅ Kafka producer started.")
+        
+        await lock_assets_producer.start()
+        await order_producer.start()
+        logger.info("✅ Kafka producers started.")
+    
     except Exception as e:
         logger.warning(f"⚠️ Ошибка Kafka: {e}")
 
     yield
+    
+    await redis_pool.close()
+    logger.info("🛑 Redis closed.")
 
-    await producer_service.stop()
+    await order_producer.stop()
+    await lock_assets_producer.stop()
+    
+    await change_balance_consumer.stop()
     await order_status_consumer.stop()
-    await ticker_consumer.stop()
+    await asset_consumer.stop()
     logger.info("🛑 Kafka Producer and Consumers stopped.")
 
 
