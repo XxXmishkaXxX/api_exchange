73720a190210a30cd8bce1918bfa099cb4697216
XxXmishkaXxX <misha.kutsov1@gmail.com>
2025-04-06 15:27:10 +0500

    add change balance consumer

--- services/orders_service/app/services/consumer.py
+++ services/orders_service/app/services/consumer.py
@@ -1,11 +1,11 @@
 import json
 from aiokafka import AIOKafkaConsumer
-from redis import asyncio as aioredis
 
 from app.core.config import settings
 from app.core.logger import logger
-from app.db.database import get_db, get_redis_connection
+from app.db.database import get_db, redis_pool
 from app.repositories.order_repo import OrderRepository
+from app.services.wallet_client import wallet_client
 
 
 class BaseKafkaConsumerService:
@@ -16,7 +16,7 @@ class BaseKafkaConsumerService:
     Классы-наследники должны реализовывать метод process_message для обработки сообщений.
     """
 
-    def __init__(self, topic: str, bootstrap_servers: str):
+    def __init__(self, topic: str, bootstrap_servers: str, group_id: str):
         """
         Инициализация потребителя Kafka.
 
@@ -26,7 +26,7 @@ class BaseKafkaConsumerService:
         """
         self.topic = topic
         self.bootstrap_servers = bootstrap_servers
-        self.consumer = AIOKafkaConsumer(self.topic, bootstrap_servers=self.bootstrap_servers)
+        self.consumer = AIOKafkaConsumer(self.topic, bootstrap_servers=self.bootstrap_servers, group_id=group_id)
 
     async def start(self):
         """Запускает потребителя Kafka."""
@@ -64,14 +64,14 @@ class OrderStatusConsumer(BaseKafkaConsumerService):
     Этот класс слушает топик 'orders_update' и обновляет статус ордера в базе данных.
     """
 
-    def __init__(self, bootstrap_servers: str):
+    def __init__(self, bootstrap_servers: str, group_id):
         """
         Инициализация потребителя статусов ордеров.
 
         Аргументы:
             bootstrap_servers (str): Адреса серверов Kafka.
         """
-        super().__init__("orders_update", bootstrap_servers)
+        super().__init__("orders_update", bootstrap_servers, group_id=group_id)
 
     async def process_message(self, message):
         """
@@ -102,22 +102,23 @@ class OrderStatusConsumer(BaseKafkaConsumerService):
                 await order_repo.update(order, {"status": status})
 
 
-class TickerConsumer(BaseKafkaConsumerService):
+class AssetConsumer(BaseKafkaConsumerService):
     """
     Потребитель Kafka для обработки тикеров.
 
     Этот класс слушает топик 'tickers' и обновляет данные о тикерах в Redis.
     """
 
-    def __init__(self, bootstrap_servers: str):
+    def __init__(self, bootstrap_servers: str, group_id: str):
         """
         Инициализация потребителя тикеров.
 
         Аргументы:
             bootstrap_servers (str): Адреса серверов Kafka.
+            group_id (str): ID группы потребителей.
         """
-        super().__init__("tickers", bootstrap_servers)
-        
+        super().__init__("tickers", bootstrap_servers, group_id=group_id)
+
 
     async def process_message(self, message):
         """
@@ -128,23 +129,63 @@ class TickerConsumer(BaseKafkaConsumerService):
         """
         data = json.loads(message.value.decode("utf-8"))
         action = data.get("action")
-        ticker_id = data.get("ticker_id")
-        symbol = data.get("symbol")
+        ticker_id = data.get("asset_id")
+        symbol = data.get("ticker")
         name = data.get("name")
 
         if action == "ADD" and ticker_id and symbol and name:
-            async with get_redis_connection() as redis:
+            async with redis_pool.connection() as redis:
                 ticker_key = f"ticker:{ticker_id}"
                 await redis.hset(ticker_key, mapping={"symbol": symbol, "name": name})
                 logger.info(f"Добавлен тикер в Redis: {ticker_id} -> {symbol} ({name})")
 
         elif action == "REMOVE" and ticker_id:
-            async with get_redis_connection() as redis:
+            async with redis_pool.connection() as redis:
                 ticker_key = f"ticker:{ticker_id}"
                 await redis.delete(ticker_key)
                 logger.info(f"Удалён тикер из Redis: {ticker_id}")
 
 
-# Создание экземпляров потребителей
-order_status_consumer = OrderStatusConsumer(settings.BOOTSTRAP_SERVERS)
-ticker_consumer = TickerConsumer(settings.BOOTSTRAP_SERVERS)
+class ChangeBalanceConsumer(BaseKafkaConsumerService):
+
+    def __init__(self, topic: str, bootstrap_servers: str, group_id: str):
+        super().__init__(topic=topic, bootstrap_servers=bootstrap_servers,
+                         group_id=group_id)
+
+    async def process_message(self, message):
+        data = json.loads(message.value.decode('utf-8'))
+
+        user_id = data["user_id"]
+        asset = data["asset"]
+        amount = int(data["amount"])
+        lock = int(data["lock"])
+
+        balance_key = f"user:{user_id}:asset:{asset}"
+
+        async with redis_pool.connection() as redis:
+            exists = await redis.exists(balance_key)
+
+            if not exists:
+                # Запрос в wallet service
+                wallet_data = await wallet_client.get_balance(user_id, asset)
+                if wallet_data is None:
+                    logger.warning(f"No wallet data found for user={user_id}, asset={asset}")
+                    return
+
+                await redis.hset(balance_key, mapping={
+                    "amount": int(wallet_data["amount"]),
+                    "lock": int(wallet_data["lock"])
+                })
+                logger.info(f"[INIT] user:{user_id}, asset:{asset}, amount:{wallet_data['amount']}, lock:{wallet_data['lock']}")
+            else:
+                await redis.hset(balance_key, mapping={
+                    "amount": amount,
+                    "lock": lock
+                })
+                logger.info(f"[UPDATE] user:{user_id}, asset:{asset}, amount:{amount}, lock:{lock}")
+        
+
+change_balance_consumer = ChangeBalanceConsumer("change_balance", settings.BOOTSTRAP_SERVERS, group_id=None)
+order_status_consumer = OrderStatusConsumer(settings.BOOTSTRAP_SERVERS, group_id=None)
+asset_consumer = AssetConsumer(settings.BOOTSTRAP_SERVERS, group_id="orders_group")
+
