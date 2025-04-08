66ef623bc784723784bdee6bd1f9ede0b4a034f2
XxXmishkaXxX <misha.kutsov1@gmail.com>
2025-04-08 23:12:14 +0500

    ref md service

--- services/market_data_service/app/routers/api/v1/md_admins.py
+++ services/market_data_service/app/routers/api/v1/md_admins.py
@@ -2,8 +2,8 @@ from fastapi import APIRouter, Depends
 
 
 from app.deps.security import admin_required
-from app.schemas.ticker import TickerSchema
-from app.services.ticker import TickerService, get_ticker_service
+from app.schemas.asset import AssetSchema
+from app.services.market_data import MarketDataService, get_market_data_service
 from app.services.producer import KafkaProducerService, get_producer_service
 
 
@@ -11,16 +11,16 @@ router = APIRouter()
 
 
 @router.post("/instrument")
-async def create_instrument(ticker: TickerSchema,
+async def create_instrument(asset: AssetSchema,
                           user_info: dict = Depends(admin_required),
-                          service: TickerService = Depends(get_ticker_service),
+                          service: MarketDataService = Depends(get_market_data_service),
                           prod: KafkaProducerService = Depends(get_producer_service)):
-    return await service.create_ticker(ticker, prod)
+    return await service.create_asset(asset, prod)
 
 
 @router.delete("/instrument/{ticker}")
 async def remove_instrument(ticker: str,
                           user_info: dict = Depends(admin_required),
-                          service: TickerService = Depends(get_ticker_service),
+                          service: MarketDataService = Depends(get_market_data_service),
                           prod: KafkaProducerService = Depends(get_producer_service)):
-    return await service.remove_ticker(ticker, prod)
+    return await service.remove_asset(ticker, prod)
--- services/market_data_service/app/routers/api/v1/md_users.py
+++ services/market_data_service/app/routers/api/v1/md_users.py
@@ -1,7 +1,14 @@
 from fastapi import APIRouter, Depends
 
 
+from app.services.market_data import MarketDataService, get_market_data_service
+
+
 
 router = APIRouter()
 
+
+@router.get("/instrument")
+async def get_assets_list(service: MarketDataService = Depends(get_market_data_service)):
+    return await service.get_list_assets()
 
