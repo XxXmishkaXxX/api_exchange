d26c224b67cc63c39d26858088be2b383e45484f
XxXmishkaXxX <misha.kutsov1@gmail.com>
2025-04-19 11:20:06 +0500

    ref asset model

--- services/market_data_service/app/models/asset.py
+++ services/market_data_service/app/models/asset.py
@@ -1,4 +1,5 @@
 from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
 
 
 from app.db.database import Base
@@ -11,6 +12,17 @@ class Asset(Base):
     ticker = Column(String(10), unique=True, nullable=False)
     name = Column(String(100), nullable=False)
 
+    order_transactions = relationship(
+        "Transaction",
+        back_populates="order_asset",
+        foreign_keys="Transaction.order_asset_id"
+    )
+    payment_transactions = relationship(
+        "Transaction",
+        back_populates="payment_asset",
+        foreign_keys="Transaction.payment_asset_id"
+    )
+
     def __repr__(self):
-        return f"<Ticker(id={self.id}, symbol={self.ticker}, name={self.name})>"
+        return f"<Asset(id={self.id}, ticker={self.ticker}, name={self.name})>"
 
