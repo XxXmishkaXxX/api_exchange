20e748a0bc7859260063b62aacc0f927910a4629
XxXmishkaXxX <misha.kutsov1@gmail.com>
2025-04-26 02:25:57 +0500

    ref user service

--- services/auth_service/app/services/user.py
+++ services/auth_service/app/services/user.py
@@ -1,40 +1,39 @@
-import jwt
 import random
 import string
 from datetime import datetime, timedelta
-from fastapi import HTTPException, Depends
-from sqlalchemy.ext.asyncio import AsyncSession
+from uuid import UUID
+
+from fastapi import HTTPException
 
 from app.repositories.user_repository import UserRepository
 from app.schemas.user import ChangePasswordRequest, ForgotPasswordRequest, ResetCodeRequest
-from app.services.email import EmailService, get_email_service
-from app.db.database import get_db
+from app.schemas.user import User
 
 
 class UserService:
-    """
-    Сервис для работы с пользователями, включающий операции аутентификации, изменения пароля и извлечения данных о текущем пользователе.
-    """
-
-    def __init__(self, user_repo: UserRepository, email_service: EmailService) -> None:
-        """
-        Инициализация сервиса пользователей.
-
-        :param user_repo: Репозиторий для работы с пользователями.
-        :param email_service: Сервис для работы с подтверждениями email.
-        """
-        self.email_service = email_service
+    def __init__(self, user_repo: UserRepository) -> None:
         self.user_repo = user_repo
 
-    async def change_password(self, user_id: int, data: ChangePasswordRequest) -> dict:
-        """
-        Меняет пароль пользователя, если старый пароль верный и оба новых пароля совпадают.
+    async def get_user(self, user_id: UUID = None, user_email: str = None):
+        user = None
+        if user_id:
+            user = await self.user_repo.get_user_by_id(user_id)
+        if user_email:
+            user = await self.user_repo.get_user_by_email(user_email)
+        if not user:
+            raise HTTPException(status_code=404, detail="Пользователь не найден")
+        return user
 
-        :param user_id: Идентификатор пользователя, чей пароль нужно изменить.
-        :param data: Данные, содержащие старый и новый пароли.
-        :return: Обновленный объект пользователя с новым паролем.
-        :raises HTTPException: Если старый пароль неверный, пароли не совпадают или произошла ошибка обновления пароля.
-        """
+    async def delete_user(self, user_id: UUID) -> User:
+        user = await self.user_repo.delete_user(user_id)
+        return User(user_id=user.id, name=user.name, role=user.role)
+    
+    async def verify_user(self, user_email: str):
+        user = await self.get_user(user_email=user_email)
+        user.is_verified = True
+        await self.user_repo.update_user(user)
+
+    async def change_password(self, user_id: UUID, data: ChangePasswordRequest) -> dict:
         user = await self.user_repo.get_user_by_id(user_id)
         if not user:
             raise HTTPException(status_code=404, detail="User not found")
@@ -45,45 +44,30 @@ class UserService:
         if data.new_password != data.new_password_confirm:
             raise HTTPException(status_code=400, detail="Passwords don't match")
 
-        updated_user = await self.user_repo.update_user(user_id, 
-                                                        password=self.user_repo.get_password_hash(data.new_password))
+        user.password = self.user_repo.get_password_hash(data.new_password)
+
+        updated_user = await self.user_repo.update_user(user)
         if not updated_user:
             raise HTTPException(status_code=500, detail="Failed to update password")
         
         return {"message": "Password has changed"}
 
     async def forgot_password(self, data: ForgotPasswordRequest):
-        """
-        Запрос на сброс пароля. Генерирует код сброса и отправляет его на email пользователя.
-
-        :param data: Запрос на сброс пароля, содержащий email пользователя.
-        :raises HTTPException: Если пользователь не найден.
-        """
         user = await self.user_repo.get_user_by_email(data.email)
-        
         if not user:
             raise HTTPException(status_code=400, detail="User not found")
         
-        reset_code = self.generate_reset_code()  # Генерация случайного кода
-        expiration_time = datetime.utcnow() + timedelta(minutes=15)  # Код действует 15 минут
+        reset_code = self.generate_reset_code()
+        expiration_time = datetime.utcnow() + timedelta(minutes=15)
 
         reset_code_entry = await self.user_repo.create_reset_code(
             user_id=user.id,
             reset_code=reset_code,
             expires_at=expiration_time
         )
-        
-        await self.email_service.send_reset_email(user.email, reset_code)
-
-        return {"detail": "Password reset code has been sent to your email."}
+        return reset_code_entry
 
     async def confirm_reset_code(self, data: ResetCodeRequest):
-        """
-        Подтверждение кода сброса и обновление пароля пользователя.
-
-        :param data: Запрос с кодом сброса и новыми паролями.
-        :raises HTTPException: Если код сброса неверный или истек.
-        """
         reset_code_entry = await self.user_repo.get_reset_code(data.code)
 
         if not reset_code_entry:
@@ -93,42 +77,21 @@ class UserService:
             raise HTTPException(status_code=400, detail="Reset code has expired")
 
         user = await self.user_repo.get_user_by_id(reset_code_entry.user_id)
-
         if not user:
             raise HTTPException(status_code=404, detail="User not found")
 
         if data.new_password != data.new_password_confirm:
             raise HTTPException(status_code=400, detail="Passwords don't match")
 
-        updated_user = await self.user_repo.update_user(user.id, password=data.new_password)
+        user.password = self.user_repo.get_password_hash(data.new_password)
+
+        updated_user = await self.user_repo.update_user(user)
         if not updated_user:
             raise HTTPException(status_code=500, detail="Failed to update password")
         
         await self.user_repo.delete_reset_code(data.code)
 
         return {"detail": "Password has been successfully reset."}
-        
+
     def generate_reset_code(self, length: int = 6) -> str:
-        """
-        Генерирует случайный код сброса пароля.
-
-        :param length: Длина кода сброса. По умолчанию 6 символов.
-        :return: Генерируемый код сброса.
-        """
         return ''.join(random.choices(string.digits + string.ascii_letters, k=length))
-
-
-
-def get_user_service(db: AsyncSession = Depends(get_db)) -> UserService:
-    """
-    Создает и возвращает сервис пользователя, инкапсулируя все зависимости, включая
-    доступ к базе данных и сервис подтверждения email.
-
-    :param db: Асинхронная сессия базы данных, автоматически передаваемая через Depends.
-    :return: Экземпляр UserService, готовый к использованию в обработчиках запросов.
-    """
-
-    email_service = get_email_service(db)
-    user_repo = UserRepository(db)
-    
-    return UserService(user_repo, email_service)
