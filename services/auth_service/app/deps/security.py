93042b0230c77889962e89823f950803e079fcff
XxXmishkaXxX <misha.kutsov1@gmail.com>
2025-04-26 15:49:24 +0500

    changed Authorization: Bearer to TOKEN

--- services/auth_service/app/deps/security.py
+++ services/auth_service/app/deps/security.py
@@ -1,13 +1,22 @@
-from fastapi import Depends, HTTPException, Security
+from fastapi import HTTPException, Header, Depends
 import jwt
-from fastapi.security import OAuth2PasswordBearer
 from jwt.exceptions import ExpiredSignatureError, InvalidSignatureError, PyJWTError
+from typing import Optional
 
 from app.core.config import settings
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="api/v1/auth/login")
 
-def get_user_from_token(token: str = Security(oauth2_scheme)) -> dict:
+def get_token(authorization: Optional[str] = Header(None)) -> str:
+    if authorization is None:
+        raise HTTPException(status_code=401, detail="Authorization token is missing")
+    
+    if not authorization.startswith("TOKEN "):
+        raise HTTPException(status_code=400, detail="Invalid token format. Expected 'TOKEN <your-token>'")
+    
+    token = authorization[len("TOKEN "):]
+    return token
+
+def get_user_from_token(token: str = Depends(get_token)) -> dict:
     try:
         payload = jwt.decode(token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM])
         user_id: str = payload.get("sub")
