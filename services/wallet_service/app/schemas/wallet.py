2815164c8200ea0894fe81a27df01e5bcc1cd501
XxXmishkaXxX <misha.kutsov1@gmail.com>
2025-04-28 16:32:49 +0500

    lil ref

--- services/wallet_service/app/schemas/wallet.py
+++ services/wallet_service/app/schemas/wallet.py
@@ -1,15 +1,13 @@
-from pydantic import BaseModel, Field, validator
-from typing import Optional
+from pydantic import BaseModel, Field
+from typing import Annotated
 from uuid import UUID
 
-
 class DepositAssetsSchema(BaseModel):
-    user_id: UUID = Field(description="id пользователя")
-    ticker: str = Field(description="Название тикера")
-    amount: int = Field(gt=0, )
+    user_id: Annotated[UUID, Field(description="id пользователя")]
+    ticker: Annotated[str, Field(description="Название тикера")]
+    amount: Annotated[int, Field(gt=0, description="Количество должно быть положительным числом")]
 
 class WithdrawAssetsSchema(BaseModel):
-    user_id: UUID = Field(description="id пользователя")
-    ticker: str = Field(description="Название тикера")
-    amount: int = Field(gt=0, )
-
+    user_id: Annotated[UUID, Field(description="id пользователя")]
+    ticker: Annotated[str, Field(description="Название тикера")]
+    amount: Annotated[int, Field(gt=0, description="Количество должно быть положительным числом")]
