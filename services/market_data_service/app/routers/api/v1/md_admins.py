e58595e3a4ba27080879d388a31081e529fe29d8
XxXmishkaXxX <misha.kutsov1@gmail.com>
2025-04-28 16:41:26 +0500

    lil ref

--- services/market_data_service/app/routers/api/v1/md_admins.py
+++ services/market_data_service/app/routers/api/v1/md_admins.py
@@ -1,4 +1,5 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Path
+from typing import List, Annotated
 
 
 from app.deps.security import admin_required
@@ -9,18 +10,21 @@ from app.services.producer import KafkaProducerService, get_producer_service
 
 router = APIRouter()
 
-
 @router.post("/instrument")
-async def create_instrument(asset: AssetSchema,
-                          user_info: dict = Depends(admin_required),
-                          service: AssetsService = Depends(get_assets_service),
-                          prod: KafkaProducerService = Depends(get_producer_service)):
+async def create_instrument(
+    asset: Annotated[AssetSchema, Depends()],
+    user_info: Annotated[dict, Depends(admin_required)],
+    service: Annotated[AssetsService, Depends(get_assets_service)],
+    prod: Annotated[KafkaProducerService, Depends(get_producer_service)]
+):
     return await service.create_asset(asset, prod)
 
 
 @router.delete("/instrument/{ticker}")
-async def remove_instrument(ticker: str,
-                          user_info: dict = Depends(admin_required),
-                          service: AssetsService = Depends(get_assets_service),
-                          prod: KafkaProducerService = Depends(get_producer_service)):
+async def remove_instrument(
+    ticker: Annotated[str, Path(description="Тикер актива")],
+    user_info: Annotated[dict, Depends(admin_required)],
+    service: Annotated[AssetsService, Depends(get_assets_service)],
+    prod: Annotated[KafkaProducerService, Depends(get_producer_service)]
+):
     return await service.remove_asset(ticker, prod)
--- services/market_data_service/app/routers/api/v1/md_users.py
+++ services/market_data_service/app/routers/api/v1/md_users.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter, Depends, Path, Query
-from typing import List
+from typing import List, Annotated
 
 from app.services.assets import AssetsService, get_assets_service
 from app.services.market_data import get_market_data_service, MarketDataService
@@ -8,60 +8,61 @@ from app.schemas.transactions import Transaction
 
 router = APIRouter()
 
-
 def get_offset(page: int, limit: int) -> int:
     return (page - 1) * limit
 
-
 @router.get("/instrument", response_model=List[dict])
-async def get_assets_list(service: AssetsService = Depends(get_assets_service)):
+async def get_assets_list(
+    service: Annotated[AssetsService, Depends(get_assets_service)]
+):
     return await service.get_list_assets()
 
 @router.get("/orderbook/{ticker}", response_model=OrderBookResponse | OrderBookErrorResponse)
 async def get_orderbook(
-    ticker: str = Path(..., description="Тикер актива (например BTC)"),
-    pair: str = Query("RUB", description="Пара актива, например USDT"),
-    limit: int = Query(10, ge=1, le=1000, description="Лимит заявок для отображения"),
-    service: MarketDataService = Depends(get_market_data_service)
+    ticker: Annotated[str, Path(description="Тикер актива (например BTC)")],
+    pair: Annotated[str, Query("RUB", description="Пара актива, например USDT")],
+    limit: Annotated[int, Query(10, ge=1, le=1000, description="Лимит заявок для отображения")],
+    service: Annotated[MarketDataService, Depends(get_market_data_service)],
 ):
     data = OrderBookRequest(ticker=ticker, pair=pair, limit=limit)
     return await service.get_orderbook(data)
 
-
 @router.get("/transactions/{ticker}", response_model=List[Transaction])
 async def get_all_transactions_by_pair(
-    ticker: str = Path(..., description="Тикер актива (например BTC)"),
-    pair: str = Query("RUB", description="Пара актива, например USDT"),
-    limit: int = Query(10, ge=1, le=1000),
-    page: int = Query(1, ge=1),
-    asset_service: AssetsService = Depends(get_assets_service),
-    market_data_service: MarketDataService = Depends(get_market_data_service)
+    ticker: Annotated[str, Path(description="Тикер актива (например BTC)")],
+    pair: Annotated[str, Query("RUB", description="Пара актива, например USDT")],
+    limit: Annotated[int, Query(10, ge=1, le=1000)],
+    page: Annotated[int, Query(1, ge=1)],
+    asset_service: Annotated[AssetsService, Depends(get_assets_service)],
+    market_data_service: Annotated[MarketDataService, Depends(get_market_data_service)],
 ):
     asset1_id, asset2_id = await asset_service.get_assets_ids_pair(ticker, pair)
-    return await market_data_service.get_transactions(asset1_id, asset2_id, limit, get_offset(page, limit))
-
+    return await market_data_service.get_transactions(
+        asset1_id, asset2_id, limit, get_offset(page, limit)
+    )
 
 @router.get("/transactions/user/{user_id}/{ticker}", response_model=List[Transaction])
 async def get_user_transactions_by_pair(
-    user_id: int = Path(..., description="ID пользователя"),
-    ticker: str = Path(..., description="Тикер актива (например BTC)"),
-    pair: str = Query("RUB", description="Пара актива, например USDT"),
-    limit: int = Query(10, ge=1, le=1000),
-    page: int = Query(1, ge=1),
-    asset_service: AssetsService = Depends(get_assets_service),
-    market_data_service: MarketDataService = Depends(get_market_data_service)
+    user_id: Annotated[int, Path(description="ID пользователя")],
+    ticker: Annotated[str, Path(description="Тикер актива (например BTC)")],
+    pair: Annotated[str, Query("RUB", description="Пара актива, например USDT")],
+    limit: Annotated[int, Query(10, ge=1, le=1000)],
+    page: Annotated[int, Query(1, ge=1)],
+    asset_service: Annotated[AssetsService, Depends(get_assets_service)],
+    market_data_service: Annotated[MarketDataService, Depends(get_market_data_service)],
 ):
     asset1_id, asset2_id = await asset_service.get_assets_ids_pair(ticker, pair)
     return await market_data_service.get_user_transactions_by_pair(
         asset1_id, asset2_id, user_id, limit, get_offset(page, limit)
     )
 
-
 @router.get("/transactions/user/{user_id}", response_model=List[Transaction])
 async def get_all_user_transactions(
-    user_id: int = Path(..., description="ID пользователя"),
-    limit: int = Query(10, ge=1, le=1000),
-    page: int = Query(1, ge=1),
-    market_data_service: MarketDataService = Depends(get_market_data_service)
+    user_id: Annotated[int, Path(description="ID пользователя")],
+    limit: Annotated[int, Query(10, ge=1, le=1000)],
+    page: Annotated[int, Query(1, ge=1)],
+    market_data_service: Annotated[MarketDataService, Depends(get_market_data_service)],
 ):
-    return await market_data_service.get_all_user_transactions(user_id, limit, get_offset(page, limit))
+    return await market_data_service.get_all_user_transactions(
+        user_id, limit, get_offset(page, limit)
+    )
