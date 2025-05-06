024a8944c80facc128b4f2e527094c076d00e5ee
Your Name <you@example.com>
2025-05-06 16:43:34 +0500

    add prod in service

--- services/auth_service/app/services/test_service.py
+++ services/auth_service/app/services/test_service.py
@@ -9,12 +9,14 @@ from app.schemas.user import User
 from app.models.test_user import UserTest
 from app.models.test_user import TestRole
 from app.core.config import settings
+from app.kafka.producers.send_wallet_event_producer import SendWalletEventProducerService
 
 class Service:
     """Сервис для тестов"""
 
-    def __init__(self, repo: TestUserRepository) -> None:
+    def __init__(self, repo: TestUserRepository, prod: SendWalletEventProducerService) -> None:
         self.repo = repo
+        self.prod = prod
 
     async def register_user(self, name: str) -> User:  
         
@@ -31,6 +33,8 @@ class Service:
 
         user = await self.repo.update_user(user)
 
+        await self.prod.send_event(user_id=user.id, event="create")
+
         return User(user_id=user.id, name=user.name, role=user.role, api_key=user.api_key)
     
     async def delete_user(self, user_id: UUID):
@@ -40,6 +44,8 @@ class Service:
         if not user:
             raise HTTPException(status_code=404, detail="User was not found")
         
+        await self.prod.send_event(user_id=user.id, event="delete")
+
         return User(user_id=user.id, name=user.name, role=user.role, api_key=user.api_key)
 
     async def create_api_key(self, user_id: UUID, role: str, minutes: int = 0, days: int = 365):
