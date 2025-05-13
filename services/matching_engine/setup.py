8902581f4ad0e300f9389f6328d79c66e50408f5
XxXmishkaXxX <misha.kutsov1@gmail.com>
2025-05-13 08:22:16 +0500

    add new paths

--- services/matching_engine/setup.py
+++ services/matching_engine/setup.py
@@ -3,7 +3,8 @@ from Cython.Build import cythonize
 
 setup(
     ext_modules=cythonize(
-        ["engine/order.pyx", "engine/order_book.pyx", "engine/matching_engine.pyx"],
+        ["engine/order.pyx", "engine/order_book.pyx", "engine/matching_engine.pyx",
+         "recovery/order_logs_replayer/logs_replayer.pyx", "recovery/snapshots_manager/manager.pyx"],
         language_level=3
     ),
 )
