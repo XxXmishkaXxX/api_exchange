af6df13c2c58b25103fb2ac32d0170ee876dccfb
Your Name <you@example.com>
2025-05-18 22:11:10 +0500

    add status in wallet model

--- services/wallet_service/app/models/wallet.py
+++ services/wallet_service/app/models/wallet.py
@@ -1,10 +1,16 @@
 from typing import List
 from sqlalchemy.dialects.postgresql import UUID
-from sqlalchemy import Index
+from sqlalchemy import Index, Enum as PG_ENUM
 from sqlalchemy.orm import mapped_column, Mapped, relationship
 from app.models.wallet_asset import WalletAssetBalance
 from app.db.database import Base
 import uuid
+from enum import Enum
+
+
+class WalletStatus(str, Enum):
+    ACTIVATE = "ACTIVATE"
+    DEACTIVATE = "DEACTIVATE"
 
 
 class Wallet(Base):
@@ -15,5 +21,6 @@ class Wallet(Base):
 
     id: Mapped[uuid.UUID] = mapped_column(UUID, primary_key=True, default=uuid.uuid4)
     user_id: Mapped[uuid.UUID] = mapped_column(UUID, index=True, nullable=False)
+    status: Mapped[WalletStatus] = mapped_column(PG_ENUM(WalletStatus), nullable=False, default=WalletStatus.ACTIVATE)
 
     asset_balances: Mapped[List["WalletAssetBalance"]] = relationship(back_populates="wallet", cascade="all, delete-orphan")
